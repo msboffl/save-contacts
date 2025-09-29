dd9bf16559329a30c116d8c59a16aae4162a6b79
Mareedu Saibabu <a3097640@bestbuy.com>
2025-09-30 00:06:03 +0530

    client & server

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
+import { Login } from "./pages/Login.tsx";
+import { Signup } from "./pages/Signup.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 
-const app = express();
+const app: Express = express();
 
-app.listen(3000, () => {
+app.get("/", (_: Request, res: Response) => {
+  res.send({ message: "API is working" });
+});
+
+app.listen(4000, () => {
   console.log("server running at port: 3000");
 });
