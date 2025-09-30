4181a3ec8e72abdd1485565f7b7323f17c22bf51
Mareedu Saibabu <a3097640@bestbuy.com>
2025-09-30 17:01:04 +0530

    server & client

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
+const HOST = process.env?.["SERVER_HOST"] || "localhost";
+const PORT = Number(process.env?.["SERVER_PORT"]) || 4000;
 
 app.get("/", (_: Request, res: Response) => {
   res.send({ message: "API is working" });
 });
 
-app.listen(4000, () => {
-  console.log("server running at port: 3000");
+app.listen(PORT, HOST, () => {
+  console.log(`[⏳⏳] Server running at http://${HOST}:${PORT}`);
 });
